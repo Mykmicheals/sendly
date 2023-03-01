b05602ceca7b6640a7adffe7ffd1a117173818b2
anon <mykmicheals@gmail.com>
2023-03-01 22:13:12 +0100

    Edited transfer modal

--- src/modals/TransactionModal.js
+++ src/modals/TransactionModal.js
@@ -1,19 +1,19 @@
-import { View, Text,Modal,TouchableOpacity } from 'react-native'
+import { View, Text,TouchableOpacity } from 'react-native'
 import React from 'react'
 import Transactions from '../components/Transactions'
+import { Modal } from 'react-native-paper'
 
 const TransactionModal = ({showModal,hideModal}) => {
   return (
     <Modal
-    animationType="slide"
-    transparent={true}
-    visible={showModal}
-    onRequestClose={hideModal}
+  visible={showModal}
+  onDismiss={hideModal}
+      contentContainerStyle = {{backgroundColor: 'white',width:'80%',height:'60%', marginHorizontal:'10%'}}
   >
     <View style={{
-         backgroundColor:'#D3D3D3',height:'100%',shadowOffset: { width: 0, height: 2 }, borderRadius:10,
+         height:'100%', borderRadius:10,
          shadowOpacity: 0.8,paddingHorizontal:'7%',paddingVertical:'6%',
-         elevation: 5,position:'absolute',width:'100%',top:0,
+        position:'absolute',width:'100%',top:0,
          
          }}>
      <Transactions />
